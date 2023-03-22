af5c1570723c9fcb571d366417d0fbaba24d40a8
wrinklykong <johnwjones1337@gmail.com>
2023-03-22 11:06:00 -0500

    Hello World! Print macros

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ static HELLO: &[u8] = b"Hello this is my OS!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
 
     loop{}
 }
