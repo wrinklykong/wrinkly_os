777c84fb782a3e5a0affc8e907449b2ac1500174
wrinklykong <johnwjones1337@gmail.com>
2023-03-22 10:25:41 -0500

    Basic macro implementation

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;         // includes vga_buffer.rs
+
+static HELLO: &[u8] = b"Hello this is my OS!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+
+    vga_buffer::print_something();
+
     loop{}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,9 +1,11 @@
 // vga buffer module
 
+use volatile::Volatile;
+use core::fmt;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
-
 pub enum Color {
     Black = 0,
     Blue = 1,
@@ -13,7 +15,7 @@ pub enum Color {
     Magenta = 5,
     Brown = 6,
     LightGray = 7,
-    DarkGray 8,
+    DarkGray = 8,
     LightBlue = 9,
     LightGreen = 10,
     LightCyan = 11,
@@ -34,7 +36,7 @@ impl ColorCode {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
+#[repr(C)]                  // tells it to mimick C ordering of struct
 struct ScreenChar {
     ascii_character: u8,
     color_code: ColorCode,
@@ -45,5 +47,67 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,            // lifetime of STATIC, aka forever/entire runtime
+}
+
+impl Writer {               // type-specific function definition
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {        // similar to C switch
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write( ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {/* TODO */}
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable range
+                _ => self.write_byte(0xfe),             // im assuming this is similar to a default return
+            }
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+pub fn print_something() {
+    use core::fmt::Write;
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
